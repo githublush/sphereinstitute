b12ad5f8e04ffc8b723d4b7210f8455666b4b33f
githublush <107060029+githublush@users.noreply.github.com>
2022-06-08 21:59:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-echo file_get_contents('https://www.hundredthousand.ml/Sphere/index.html');
-?>
+
+// Comment these lines to hide errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require 'includes/config.php';
+require 'includes/functions.php';
+
+init();
